3218dc830fa8ed606156f0dd506e1d3e4d8cfc17
Carlos Ochoa <charlangas@gmail.com>
2014-03-31 17:44:14 -0600

    Add pins/users association

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting2::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Note to update this to actual URL
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
